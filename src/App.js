1ad2e110f7b21d1ac406f975c3745873f0d12db9
Marcario <christinenalubwama4@gmail.com>
2023-03-31 12:44:06 +0300

    menu

---
+++ src/App.js
@@ -0,0 +1,42 @@
+import React, { useState, } from "react";
+import Menu from "./Components/menu";
+import Categories from "./Components/categories";
+import items from "./Components/data";
+import logo from "./Components/logo.png";
+
+const allCategories = ["all", ...new Set(items.map((item) => item.category))];
+
+const App = () => {
+  const [menuItems, setMenuItems] = useState(items);
+  const [activeCategory, setActiveCategory] = useState("");
+  const [categories,] = useState(allCategories);
+
+  const filterItems = (category) => {
+    setActiveCategory(category);
+    if (category === "all") {
+      setMenuItems(items);
+      return;
+    }
+    const newItems = items.filter((item) => item.category === category);
+    setMenuItems(newItems);
+  };
+  return (
+    <main>
+      <section className="menu section">
+        <div className="title">
+          <img src={logo} alt="logo" className="logo" />
+          <h2>Menu List</h2>
+          <div className="underline"></div>
+        </div>
+        <Categories
+          categories={categories}
+          activeCategory={activeCategory}
+          filterItems={filterItems}
+        />
+        <Menu items={menuItems} />
+      </section>
+    </main>
+  );
+};
+
+export default App;
---
+++ src/Components/data.js
@@ -0,0 +1,129 @@
+
+
+
+const menu = [
+    {
+      id: 1,
+      title: 'LOADED CHAPATI COMBO',
+      category: 'breakfast',
+      price:31000,
+      img: './images/image1.jpg',
+      desc: `Chapatti, fried spinach, 2 Sausages, 2 eggs served with Home fries and Baked Beans. `,
+    },
+    {
+      id: 2,
+      title: 'FRIED CRISPY CHICKEN WINGS',
+      category: 'lunch',
+      price:33500,
+      img: './images/image6.jpg',
+      desc: `Marinated chicken wings covered with bread crumbs and deep fried, served with carrots and dipping sauce `,
+    },
+    {
+      id: 3,
+      title: 'OREO MILKSHAKE',
+      category: 'shakes',
+      price:17500,
+      img: './images/image16.jpg',
+      desc: `Blend of premium vanilla ice cream, milk & oreo cookies.`,
+    },
+    {
+      id: 4,
+      title: 'CHICKEN & EGG WRAP COMBO',
+      category: 'breakfast',
+      price:31000,
+      img: './images/image2.jpg',
+      desc: `Grilled chicken, farm fresh eggs, avocado, cheese and fresh tomato served in a home-made tortilla served with a fruit bowl, salsa and home fries. `,
+    },
+    {
+      id: 5,
+      title: 'CARIBBEAN JERK CHICKEN',
+      category: 'lunch',
+      price: 41000,
+      img: './images/image7.jpg',
+      desc: `Marinated chicken on bone Caribbean style, grilled to perfection, served with salsa, plantains, salad, 1000 island dressing and ranchero sauce. With your choice of fries or rice or mashed potatoes.`,
+    },
+    {
+      id: 6,
+      title: 'STRAWBERRY MILKSHAKE',
+      category: 'shakes',
+      price:16500,
+      img: './images/image9.jpg',
+      desc: `Strawberry Milkshake`,
+    },
+    
+    {
+        id: 7,
+        title: 'KIDDIE JUICES',
+        category: 'drinks',
+        price: 10000,
+        img: './images/image11.jpg',
+        desc: `Fresh juice of your choice of either passion, mango, pineapple, watermelon or carrot juice.`,
+      },
+    {
+      id: 8,
+      title: 'GRILLED CHICKEN & EGGS',
+      category: 'breakfast',
+      price:31000,
+      img: './images/image3.jpg',
+      desc: `Grilled chicken breasts, two farm fresh eggs served with golden brown home fries and toast.`,
+    },
+    {
+      id: 9,
+      title: 'lusaniya',
+      category: 'lunch',
+      price: 35000,
+      img: './images/image14.jpg',
+      desc: ` Fast food Lusaniya with chicken and goats meat `,
+    },
+    {
+      id: 10,
+      title: 'BLUEBERRY MILKSHAKE',
+      category: 'shakes',
+      price: 17000,
+      img: './images/image8.jpg',
+      desc: `A blend of premium vanilla ice cream, milk & Blueberries.`,
+    },
+    {
+        id: 11,
+        title: ' FIT BREAKFAST',
+        category: 'breakfast',
+        price:31000,
+        img: './images/image4.jpg',
+        desc: `Grilled Chicken breast, Avocado, Poached eggs, steamed spinach and roasted plantain & Toast`,
+      },
+      {
+        id: 12,
+        title: 'BREAKFAST PANCAKE SAMPLER',
+        category: 'breakfast',
+        price:31000,
+        img: './images/image5.jpg',
+        desc: `2 signature pancakes, two farm fresh eggs, two sausages, baked beans and golden home fries.`,
+      },
+
+      {
+        id: 13,
+        title: 'ICED DAWA TEAS',
+        category: 'drinks',
+        price: 12000,
+        img: './images/image10.jpeg',
+        desc: `Your favourite Dawa teas served in ice`,
+      },
+      {
+        id: 14,
+        title: 'CAFÉ MOCHA',
+        category: 'drinks',
+        price:14500,
+        img: './images/image12.jpg',
+        desc: `Espresso with chocolate and froth milk.`,
+      },
+      {
+        id: 15,
+        title: 'ICED COFFEE',
+        category: 'drinks',
+        price:9500,
+        img: './images/image13.jpg',
+        desc: `Coffee on ice.`,
+      }
+      
+  ];
+  export default menu;
